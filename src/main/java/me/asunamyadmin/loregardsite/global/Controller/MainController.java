f03048ce81d0bab82d6b46766bb0c7fa80e6e44b
AsunaMYAdmin <kiritoadm@mail.ru>
2026-02-22 12:31:41 +0700

    deleted application.properties

--- src/main/java/me/asunamyadmin/loregardsite/global/Controller/MainController.java
+++ src/main/java/me/asunamyadmin/loregardsite/global/Controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping
     public String mainPage(){
         return "index";
@@ -12,6 +13,6 @@ public class MainController {
 
     @GetMapping("/bank")
     public String bankPage(){
-        return "redirect:https://bank.loregard.ru";
+        return "redirect:BANK_URL";
     }
 }
