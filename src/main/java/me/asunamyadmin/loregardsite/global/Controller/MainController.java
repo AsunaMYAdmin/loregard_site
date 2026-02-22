10a61000f1d921dc5550763fbd390bb0a0223509
AsunaMYAdmin <kiritoadm@mail.ru>
2026-02-22 17:14:44 +0700

    Added main CSS + HTML

--- src/main/java/me/asunamyadmin/loregardsite/global/Controller/MainController.java
+++ src/main/java/me/asunamyadmin/loregardsite/global/Controller/MainController.java
@@ -2,6 +2,7 @@ package me.asunamyadmin.loregardsite.global.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,13 +12,104 @@ public class MainController {
 
 
 
-    @GetMapping
-    public String mainPage(){
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("activePage", "home");
         return "index";
     }
 
     @GetMapping("/bank")
     public String bankPage(){
         return "redirect:" + bankURL;
+    }
+
+    @GetMapping("/profile")
+    public String profilePage(Model model) {
+        model.addAttribute("activePage", "profile");
+        return "profile";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("activePage", "about");
+        return "about";
+    }
+
+    @GetMapping("/rules")
+    public String rulesPage(Model model) {
+        model.addAttribute("activePage", "rules");
+        return "rules";
+    }
+
+    @GetMapping("/heroes")
+    public String topPage(Model model) {
+        model.addAttribute("activePage", "heroes");
+        return "heroes";
+    }
+
+    @GetMapping("/guilds")
+    public String guildsPage(Model model) {
+        model.addAttribute("activePage", "guilds");
+        return "guilds";
+    }
+
+    @GetMapping("/faq")
+    public String faqPage(Model model) {
+        model.addAttribute("activePage", "faq");
+        return "faq";
+    }
+
+    @GetMapping("/contacts")
+    public String contactPage(Model model) {
+        model.addAttribute("activePage", "contacts");
+        return "contacts";
+    }
+
+    @GetMapping("/support")
+    public String supportPage(Model model) {
+        model.addAttribute("activePage", "support");
+        return "support";
+    }
+
+    @GetMapping("/shop")
+    public String shopPage(Model model) {
+        model.addAttribute("activePage", "shop");
+        return "development";
+    }
+
+    @GetMapping("/map")
+    public String mapPage(Model model) {
+        model.addAttribute("activePage", "map");
+        return "development";
+    }
+
+    @GetMapping("/wiki")
+    public String wikiPage(Model model) {
+        model.addAttribute("activePage", "shop");
+        return "development";
+    }
+
+    @GetMapping("/forum")
+    public String forumPage(Model model) {
+        model.addAttribute("activePage", "shop");
+        return "development";
+    }
+
+    @GetMapping("/banlist")
+    public String banlistPage(Model model) {
+        model.addAttribute("activePage", "banlist");
+        return "banlist";
+    }
+
+    @GetMapping("/staff")
+    public String staffPage(Model model) {
+        model.addAttribute("activePage", "staff");
+        return "staff";
+    }
+
+    @GetMapping("/play")
+    public String launcherPage(Model model) {
+        model.addAttribute("activePage", "play");
+        return "play";
     }
 }
