0e04912520dd538cf3ae88c4e4b7391941267a93
AsunaMYAdmin <kiritoadm@mail.ru>
2026-02-22 12:04:27 +0700

    Added Main HTML+CSS, Added redirect in bank

---
+++ src/main/java/me/asunamyadmin/loregardsite/LoregardSiteApplication.java
@@ -0,0 +1,13 @@
+package me.asunamyadmin.loregardsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoregardSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoregardSiteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/asunamyadmin/loregardsite/global/Controller/MainController.java
@@ -0,0 +1,17 @@
+package me.asunamyadmin.loregardsite.global.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String mainPage(){
+        return "index";
+    }
+
+    @GetMapping("/bank")
+    public String bankPage(){
+        return "redirect:https://bank.loregard.ru";
+    }
+}
